847aa1dbe522721e94fadf6c87e4db27348ee308
Navat2007 <navat2007@yandex.ru>
2024-07-12 15:45:43 +0300

    change to dragon

--- Scripts/characters/character.gd
+++ Scripts/characters/character.gd
@@ -1,10 +1,11 @@
 class_name Character extends Node2D
 
 @export var audio_player: AudioStreamPlayer;
-@export var animation_player: AnimationPlayer;
-@export var animated_sprite: AnimatedSprite2D;
 @export var music_clip: AudioStream;
 @export var click_area: Area2D;
+
+@onready var dragonbones:DragonBones = $DragonBones;
+@onready var _armature :DragonBonesArmature = dragonbones.armature
 
 enum States {
     Idle,
@@ -25,43 +26,40 @@ func _ready() -> void:
     else:
         print("No music clip provided for character: ", name);
 
-    animated_sprite.self_modulate = Color(0.176, 0.176, 0.176, 0.812);
+    _armature.current_animation = "idle";
+    modulate = Color(0.176, 0.176, 0.176, 0.812);
 
 func _on_click_area_input_event(_viewport, event, _shape_idx) -> void:
     if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+        _armature.stop(_armature.current_animation, true)
+
         match current_state:
             States.Idle:
-                current_state = States.Ready
-                animated_sprite.self_modulate = Color(1.0, 1.0, 1.0, 1.0);
+                current_state = States.Ready;
+                modulate = Color(1.0, 1.0, 1.0, 1.0);
 
-                var start_frame: int = floori(Global.cycle_elapsed / time_per_frame) % total_frames;
+                #var start_frame: int = floori(Global.cycle_elapsed / time_per_frame) % total_frames;
+                var start_progress: float = Global.cycle_elapsed / time_per_frame / 100;
 
-                animated_sprite.play("ready")
-                animated_sprite.set_frame(start_frame);
+                _armature.play_from_progress("ready", start_progress);
             States.Play:
-                current_state = States.Idle
-                animated_sprite.play("idle")
-                animated_sprite.self_modulate = Color(0.176, 0.176, 0.176, 0.812);
+                current_state = States.Idle;
+                _armature.play("idle");
+                modulate = Color(0.176, 0.176, 0.176, 0.812);
                 audio_player.stop()
             States.Ready:
-                current_state = States.Idle
-                animated_sprite.play("idle")
-                animated_sprite.self_modulate = Color(0.176, 0.176, 0.176, 0.812);
+                current_state = States.Idle;
+                _armature.play("idle");
+                modulate = Color(0.176, 0.176, 0.176, 0.812);
                 audio_player.stop()
 
 func _on_cycle_tick() -> void:
     match current_state:
         States.Idle:
-            pass
+            pass;
         States.Play:
-            audio_player.play()
+            audio_player.play();
         States.Ready:
-            current_state = States.Play
-            animated_sprite.play("play")
-            audio_player.play()
-
-func fade_in() -> void:
-    pass
-
-func fade_out() -> void:
-    pass
+            current_state = States.Play;
+            _armature.play("play");
+            audio_player.play();
